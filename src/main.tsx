7960b2a07ace816b684db110a6737edf202337b1
surafel <surafelmengist2015@gmail.com>
2025-12-21 11:42:54 +0300

    update day 25

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+// main.tsx or index.css
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/css/bootstrap.css"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- src/omponents/Projects/Increment/Increment.tsx
+++ src/omponents/Projects/Increment/Increment.tsx
@@ -1,30 +1,55 @@
 
 import { useState } from "react";
 
+const Increment: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
 
+  const increment = (): void => {
+    setValue(prev => prev + 1);
+  };
 
-const Increment = () => {
-    const [value,setValue] =useState(0)
-  const dec=()=>{
-  setValue(value+1)
-  }
-  const res=() =>[
-    setValue(0)
-  ]
-  const inc=()=>{
-    setValue(value-1)
-  }
+  const decrement = (): void => {
+    setValue(prev => prev - 1);
+  };
+
+  const reset = (): void => {
+    setValue(0);
+  };
+
   return (
-    
-    <div className="bg-indigo-900 text-white h-screen flex justify-center items-center ">
-        <section>
-            <button className="bg-green-700 text-white rounded px-8 py-4 text-center text-3xl m-4 hover:bg-green-500" onClick={dec}>Decrement</button>
-            <button className="bg-green-700 text-white rounded px-8 py-4 text-center text-3xl m-4 hover:bg-green-500" onClick={res}>Reset</button>
-            <span className="font-bold text-3xl text-orange-950 ">{value}</span>
-            <button className="bg-green-700 text-white rounded px-8 py-4 text-center text-3xl m-4 hover:bg-green-500" onClick={inc}>Increment</button>
-        </section>
+    <div className="bg-indigo-900 text-white h-screen flex justify-center items-center">
+      <section>
+        <button
+          className="bg-green-700 rounded px-8 py-4 text-3xl m-4 hover:bg-green-500"
+          onClick={decrement}
+        >
+          Decrement
+        </button>
+
+        <button
+          className="bg-green-700 rounded px-8 py-4 text-3xl m-4 hover:bg-green-500"
+          onClick={reset}
+        >
+          Reset
+        </button>
+
+        <span className="font-bold text-3xl text-orange-300 mx-4">
+          {value}
+        </span>
+
+        <button
+          className="bg-green-700 rounded px-8 py-4 text-3xl m-4 hover:bg-green-500"
+          onClick={increment}
+        >
+          Increment
+        </button>
+
+        <button className="btn btn-info px-4 py-2 rounded text-3xl">
+          <i className="fa-solid fa-house"></i> Info
+        </button>
+      </section>
     </div>
-  )
+  );
 };
 
 export default Increment;

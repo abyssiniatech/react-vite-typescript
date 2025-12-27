e9dcf6c79572fb950c043fc009cd672f2d92b20f
surafel <surafelmengist2015@gmail.com>
2025-12-27 11:27:59 +0300

    day 30

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [data, setData] = useState<{id: number, name: string}[]>([]);
+  useEffect(() => {
+    setTimeout(async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setData(data);
+    }, 1000);
+  }, []);
+  return <div className="bg-indigo-800 min-h-screen text-white p-10">
+       <h1 className="text-2xl text-center">User list</h1>
+       {
+
+        data.map((user: {id: number, name: string}) => (
+          <p className="border border-gray-400 flex flex-col align-items-center rounded p-4 justify-center hover:bg-indigo-700" key={user.id}>{user.name}</p>
+        ))
+       }
+  </div>;
 };
 
 export default App;

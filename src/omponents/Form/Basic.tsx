e5bf1b97dcedab9159bedf9b568544405ac62865
surafel <surafelmengist2015@gmail.com>
2025-12-27 14:14:52 +0300

    day fetch data fro  json Api

--- src/omponents/Form/Basic.tsx
+++ src/omponents/Form/Basic.tsx
@@ -2,38 +2,40 @@ import React, { useState } from "react";
 type FormDataPrps = {
     name: string;
     email: string;
-
 }
 const forminput ={
     name: '',
     email: ''
 }
 const Basic = () => {
-//    control the form elements
 const [formData, setFormData] = useState<FormDataPrps>(forminput);
-
+const [age, setAge] = useState<number>(0);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
-    }));
-    // reset form after submission
-      
+      [name]: value,
+    })); 
+    //  clear form input after submit
+    
+        
+     
+
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(formData);
-    setFormData(forminput);
+    console.log(formData.name, formData.email, age);
   };
-
+  const handleAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(Number(e.target.value));
+    };
   return (
     <div className="m-4 p-4 border h-screen">
          <header className="p-4 m-4">
-             {/* render name and email */}
                 <h2 className="text-2xl font-bold mb-4">Name: {formData.name}</h2>
                 <h2 className="text-2xl font-bold mb-4">Email: {formData.email}</h2>
+                <h2 className="text-2xl font-bold mb-4">Age: {age}</h2>
          </header>
           <form onSubmit={handleSubmit} className="bg-indigo-800 text-white font-bold p-4 m-2 rounded shadow-md">
             <label htmlFor="name">Name:</label>
@@ -42,9 +44,11 @@ const [formData, setFormData] = useState<FormDataPrps>(forminput);
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
             <br />
+             <label htmlFor="age">Age</label>
+            <input type="number" id="age" name="age" value={age} onChange={handleAge} />
             <button className="bg-white text-indigo-800 font-bold py-2 px-4 rounded hover:bg-gray-200" type="submit">Submit</button>
-
-          </form>
+           
+           </form>
     </div>
   )
 };

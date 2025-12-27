6b4f178c9390381deaf654225865d073ef0022e6
surafel <surafelmengist2015@gmail.com>
2025-12-27 13:54:28 +0300

    day fetch data fro  json Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-
-import { useEffect, useState } from "react";
+import Basic from "./omponents/Form/Basic"; 
 
 const App = () => {
-  const [data, setData] = useState<{id: number, name: string}[]>([]);
-  useEffect(() => {
-    setTimeout(async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      const data = await response.json();
-      setData(data);
-    }, 1000);
-  }, []);
-  return <div className="bg-indigo-800 min-h-screen text-white p-10">
-       <h1 className="text-2xl text-center">User list</h1>
-       {
-
-        data.map((user: {id: number, name: string}) => (
-          <p className="border border-gray-400 flex flex-col align-items-center rounded p-4 justify-center hover:bg-indigo-700" key={user.id}>{user.name}</p>
-        ))
-       }
-  </div>;
+  return (
+    <div>
+       <Basic />
+    </div>
+  )
 };
 
 export default App;
---
+++ src/omponents/Form/Basic.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+type FormDataPrps = {
+    name: string;
+    email: string;
+
+}
+const forminput ={
+    name: '',
+    email: ''
+}
+const Basic = () => {
+//    control the form elements
+const [formData, setFormData] = useState<FormDataPrps>(forminput);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+    // reset form after submission
+      
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(forminput);
+  };
+
+  return (
+    <div className="m-4 p-4 border h-screen">
+         <header className="p-4 m-4">
+             {/* render name and email */}
+                <h2 className="text-2xl font-bold mb-4">Name: {formData.name}</h2>
+                <h2 className="text-2xl font-bold mb-4">Email: {formData.email}</h2>
+         </header>
+          <form onSubmit={handleSubmit} className="bg-indigo-800 text-white font-bold p-4 m-2 rounded shadow-md">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+            <br />
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+            <br />
+            <button className="bg-white text-indigo-800 font-bold py-2 px-4 rounded hover:bg-gray-200" type="submit">Submit</button>
+
+          </form>
+    </div>
+  )
+};
+
+export default Basic;

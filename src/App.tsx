f8ac4b4c3da4f7276a39cf99e90f020c4188d3e1
surafel <surafelmengist2015@gmail.com>
2025-12-29 21:26:22 +0300

    intial commit  for day 32

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Parent1 from "./omponents/fundamentals/Props/Parent1";
-import "/src/App.css"
-const App = () => {
+type Props = {
+  name:string,
+  age:number,
+  isStudent:boolean
+}
+const info: Props = {
+  name: "John Doe",
+  age: 25,
+  isStudent: true
+}
+const App = (info: Props) => {
   return (
-    <div>
-      <Parent1 />
-      
+    <div className=" p-4 bg-indigo-900 text-white h-screen flex justify-center ">
+      <main className="  flex justify-center flex-col p-4 items-center rounded-md border border-gray-400 items-center bg-indigo-600">
+        <h2 className="text-3xl font-bold mb-4">Welcome to the App Component</h2>
+      <h1>{info.name}</h1>
+      <p>Age: {info.age}</p>
+      <p>Is Student: {info.isStudent ? "Yes" : <p className="text-red-700">No</p>}</p>
+            </main>
     </div>
+    
   )
 };
 

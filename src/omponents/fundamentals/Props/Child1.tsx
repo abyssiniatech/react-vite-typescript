0284679e49e5bcc5ecb8ab7ba8a3a37459736bf6
surafel <surafelmengist2015@gmail.com>
2025-12-29 21:11:46 +0300

    intial commit  for day 32

--- src/omponents/fundamentals/Props/Child1.tsx
+++ src/omponents/fundamentals/Props/Child1.tsx
@@ -1,15 +1,15 @@
 type Child1Props = {
-    message:React.ComponentType;
-}
+  message: React.ComponentType;
+  children: React.ReactNode;
+};
 
 const Child1 = (props: Child1Props) => {
-    
   return (
     <div>
-       <props.message />
+      <props.message />
+      {props.children}
     </div>
-  
-  )
+  );
 };
 
 export default Child1;
--- src/omponents/fundamentals/Props/Parent1.tsx
+++ src/omponents/fundamentals/Props/Parent1.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import Child1 from "./Child1";
+import Button from "./Button";
 
-function Hello(){
-    return <h1>Hello this is the parent components</h1>
+function Hello() {
+  return <h1>Hello this is the parent component</h1>;
 }
 
 const Parent1 = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
       <div>Parent1</div>
-      <Child1 message={Hello} />
+
+      <Child1 message={Hello}>
+        <section>
+          <p>this is the children prop in React + TypeScript</p>
+
+          <Button
+            className="btn btn-primary"
+            onClick={() => setCounter(counter + 1)}
+            Counter={counter}
+            setCounter={setCounter}
+          >
+            Increment {counter}
+          </Button>
+        </section>
+      </Child1>
     </>
-  )
+  );
 };
 
 export default Parent1;

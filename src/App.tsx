c8a34028ca187b7531cfed6404e889738f816097
surafel <surafelmengist2015@gmail.com>
2025-12-30 04:38:04 +0300

    intial commit  for day 32

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import Sum from "./omponents/fundamentals/Props/Practice";
+
+import Show from "./omponents/Project/Show";
+
 const App = () => {
-  const [count, seCount] = useState<number>(0);
+  
   return (
     <div>
-      <Sum a={1} b={2}>
-        <p>{count}</p>
-         <button onClick={()=>seCount(count+1)} className="btn btn-primary text-2xl p-4 border-0 ">Children</button>
-      </Sum>
+     
+      <Show />
     </div>
   )
 };
--- src/omponents/fundamentals/Props/Practice.tsx
+++ src/omponents/fundamentals/Props/Practice.tsx
@@ -1,16 +1,19 @@
-type Props = {
-    a:number;
-    b:number;
-    children?: React.ReactNode;
-}
 
-const Sum = ({a,b,children}: Props) => {
+type PropsSurafel = {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const Practice = ({count,setCount}:PropsSurafel ) => {
+       const handleInc =()=>{
+         setCount(count+1)
+     }
   return (
-    <div className="text-center bg-indigo-700 text-3xl text-white font-bold">
-        <p>The sum of two number is  = {a + b}</p>
-        {children}
-        </div>
+    <div>
+        <p>{count}</p>
+        <button onClick={handleInc}>Increment</button>
+    </div>
   )
 };
 
-export default Sum;
+export default Practice;

c6ec54d7215deb549458f09ce7286f007f2148d9
surafel <surafelmengist2015@gmail.com>
2026-01-03 21:27:33 +0300

    add  active style in react router

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -2,12 +2,19 @@
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  const handleactive=({isActive}:{isActive:boolean})=>{
+    return {
+       fontWeignt:isActive ? "bold" : "normal",
+       textDecoration: isActive? "underLine": "none",
+       color:isActive ? "pink": "white"
+    }
+  }
   return (
     <div className="p-2  flex items-center gap-4 justify-center">
         
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink style={handleactive} to="/">Home</NavLink>
+            <NavLink style={handleactive} to="/about">About</NavLink>
+            <NavLink style={handleactive} to="/contact">Contact</NavLink>
   
        
     </div>

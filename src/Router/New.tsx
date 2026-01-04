1d90890dedbaa18c26ccce30c98e5dd1112cdf11
surafel <surafelmengist2015@gmail.com>
2026-01-04 18:08:07 +0300

    Add useParms hook in the projects

--- src/Router/New.tsx
+++ src/Router/New.tsx
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
 
+type Params = {
+  id: string;
+};
 
 const New = () => {
+  const { id } = useParams<Params>();
+  if (!id) {
+    return <p>No product found</p>;
+  }
   return (
-    <div>New products</div>
-  )
+    <div>New product ID: {id}</div>
+  );
 };
 
 export default New;
--- src/Router/Products.tsx
+++ src/Router/Products.tsx
@@ -1,13 +1,15 @@
-import { NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet, useNavigate } from "react-router-dom"
 
 const Products =()=>{
+    const navigate=useNavigate()
     return(
         <div className="bg-teal-700 text-white rounded ">
          <input type="text" placeholder="enter the products" />
            <nav className="bg-indigo-900 text-white flex gap-4 items-center justify-center p-4 mt-4">
 
-          <NavLink className="bg-green-800 text-white px-6 py-3 rounded text-2xl" to="new">New product</NavLink>
-          <NavLink className="bg-green-800 text-white px-6 py-3 rounded text-2xl" to="feature">Feature products</NavLink>
+            <NavLink  className="bg-green-800 text-white px-6 py-3 rounded text-2xl" to="new">New product</NavLink>
+            <NavLink  className="bg-green-800 text-white px-6 py-3 rounded text-2xl" to="feature">Feature products</NavLink>
+             <button onClick={()=>navigate('/')}>Go to home</button>
            </nav>
            <Outlet />
         </div>

15bb038a16f5b7b3be54b9020200411940e2d805
surafel <surafelmengist2015@gmail.com>
2026-01-06 19:34:06 +0300

    add useref input ref

--- src/components/hooks/useref/Second.tsx
+++ src/components/hooks/useref/Second.tsx
@@ -8,8 +8,8 @@ const Second = () => {
     }
   return (
     <div className="h-screen flex items-center justify-center gap-8">
-        <input className="border  px-8 py-4 text-2xl" type="text" ref={inputRef} />
-        <button className="bg-blue-700 text-2xl text-white px-8 py-4 rounded text-center" onClick={handleref}>Focus</button>
+        <input className="rounded-md  px-8 py-4 text-2xl" type="text" ref={inputRef} />
+        <button className="bg-blue-700 text-2xl text-white px-8 py-4 rounded-lg text-center" onClick={handleref}>Focus</button>
     </div>
   )
 };
